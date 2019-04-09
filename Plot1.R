2bc344aa7191d925371d18dbe9dae7bda0421ed3
MeiChingGoo <48003249+MeiChingGoo@users.noreply.github.com>
2019-04-09 13:21:28 +0800

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,27 @@
+install.packages("data.table")
+#set working directory
+setwd("D:/Lab7")
+
+#load library data.table
+library(data.table)
+
+DT<-fread("D:/Lab7/household_power_consumption.txt", na.strings="?")
+
+#set date as d:m:y
+DT$strDate <- as.Date(DT$Date,"%d/%m/%Y")
+
+#select data 
+DT.filter<-DT[DT$strDate=="2007-02-01" | DT$strDate=="2007-02-02",]
+DT.filter$Global_active_power<-as.numeric(DT.filter$Global_active_power)
+rm(DT)
+
+#output histogram to PNG
+png(filename="plot1.png", width=480, height=480)
+hist(DT.filter$Global_active_power, 
+     col="red", 
+     xlab="Global Active Power (kilowatts)", 
+     main="Global Active Power")
+dev.off()
+
+
+
